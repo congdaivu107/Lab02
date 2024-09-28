10e4372162c956e1c79e961cd67fa5ef142a0e6e
congdaivu107 <congdaivu107@gmail.com>
2024-09-29 04:22:02 +0700

    update test1

--- test1/App.js
+++ test1/App.js
@@ -1,19 +1,17 @@
-import React, {useState} from 'react'
-import { Text, View, Button } from 'react-native'
+import React, { useState } from "react";
+import { Text, View, Button } from "react-native";
 export default function App() {
   const [count, setCount] = useState(0);
-  
-    return (
+
+  return (
     <View>
-        <Button
-          title = "Count"
-          onPress = {() => {
-            setCount(preCount => preCount + 1)
-          }}
-          />
-          <Text>
-            Count: {count}
-        </Text>
+      <Button
+        title="Count"
+        onPress={() => {
+          setCount((preCount) => preCount + 1);
+        }}
+      />
+      <Text>Count: {count}</Text>
     </View>
-    )
+  );
 }
